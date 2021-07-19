db3ce192741b24f330699b2b04f75a11b7c5f12e
Francois Janson <jansonfrancois@gmail.com>
2021-07-19 12:21:49 +0200

    Exercises update

--- DOM_exercises/Formulaire_admin_user/main.js
+++ DOM_exercises/Formulaire_admin_user/main.js
@@ -1,8 +1,6 @@
 var btn = document.getElementById("submit");
 var adminBlock = document.getElementById("admin-block");
 
-var isAdmin;
-
 // console.log(btn);
 // console.log(adminBlock);
 
@@ -13,15 +11,8 @@ btn.onclick = function() {
     if (pass === "" || log === "") {
         alert("Enter login and password")
     } else if (log === "admin" && pass === "admin") {
-        isAdmin = true;
         adminBlock.style.display = "block";
     } else {
-        isAdmin = false;
+        adminBlock.style.display = "none"
     }
 };
-
-if (isAdmin != true) {
-    adminBlock.style.display = "none";
-} else {
-    adminBlock.style.display = "block";
-}
--- DOM_exercises/TP_Vente_en_ligne/main.js
+++ DOM_exercises/TP_Vente_en_ligne/main.js
@@ -6,32 +6,81 @@ var numberSelect = document.getElementById("numberSelect");
 var pArticle = document.getElementById("articleP");
 var pColor = document.getElementById("colorP");
 var pNumber = document.getElementById("numberP");
+var pResult = document.getElementById("resultP");
+var price = 0;
+var article = "Article";
+var color = "Couleur";
+var number = 0;
+
 
 
 articleSelect.addEventListener("change", function() {
     article = articleSelect.value;
+    if (article === "Article") {
+        pArticle.innerHTML = "";
+        return false;
+    }
     pArticle.innerHTML = "Vous avez choisi l'article : " + article;
 
 })
 
 colorSelect.addEventListener("change", function() {
     color = colorSelect.value;
+    if (color === "Couleur") {
+        pColor.innerHTML = "";
+        return false
+    }
     pColor.innerHTML = "Vous avez choisi la couleur : " + color;
 
 })
 
 numberSelect.addEventListener("change", function() {
     number = numberSelect.value;
-    pNumber.innerHTML = "Vous avez choisi le nombre : " + number;
+    if (number === 0) {
+        pNumber.innerHTML = "";
+        return false
+    }
+    pNumber.innerHTML = "Nombre d'articles : " + number;
 
 })
 
 
+
 btn.addEventListener("click", function() {
 
+    if (article === "Article" || color === "Couleur" || number === 0) {
 
+        alert("L'ensemble des champs ne sont pas saisis !");
+        return false;
+    }
+    let articleIndex = 0;
+    console.log(article);
+    console.log(number);
 
+    switch (article) {
+        case "veste":
+            articleIndex = 0;
+            break;
+        case "pantalon":
+            articleIndex = 1;
+            break;
+        case "chemise":
+            articleIndex = 2;
+            break;
+        case "pull":
+            articleIndex = 3;
+            break;
+        case "manteau":
+            articleIndex = 4;
+            break;
+    }
 
+    let articlePrice = pricesArray[articleIndex];
+    console.log(articlePrice);
+
+    price = (articlePrice * number) + (articlePrice * number * 0.2);
+
+    pResult.innerHTML = "Le montant de votre commande s'élève à " + price + "€ TTC";
 
 
 });
