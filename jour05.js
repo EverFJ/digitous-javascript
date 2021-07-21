7f71cd57d2671b0e9dd05bc721c45055ef1efd69
Francois Janson <jansonfrancois@gmail.com>
2021-07-21 11:07:29 +0200

    Exercises update

--- jour05.js
+++ jour05.js
@@ -15,35 +15,3 @@ console.log(multiply(8));
 
 console.log(addition(10));
 
-// 03 Prompt
-
-mysteryNum = Math.floor(Math.random() * (100 + 1) + 1);
-// console.log(mysteryNum);
-
-function play() {
-    prompt.start();
-    var question = "Quel est le nombre mystère ?"
-    prompt.get(question, function(err, nb) {
-        // if (err) {
-        //     return onerror(err)
-        // }
-
-        // if (nb > ) {
-        //     console.log("error");
-        //     return false
-        // }
-
-        if (nb[question] == mysteryNum) {
-            console.log("Bravo !!!");
-        } else if (nb[question] > mysteryNum) {
-            console.log("C'est moins...");
-            play();
-        } else if (nb[question] < mysteryNum) {
-            console.log("C'est plus...");
-            play();
-        }
-
-    })
-}
-
-play();
