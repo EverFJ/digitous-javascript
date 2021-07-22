17820653cf2ba0ae7709d46b23131ae706c9bc48
Francois Janson <jansonfrancois@gmail.com>
2021-07-22 11:20:04 +0200

    Exercises update

--- regex.js
+++ regex.js
@@ -22,8 +22,9 @@ let string2 = "My age is 35"
 regex = /my age is [0-9]+/i
 regex2 = /my age is [0-9]/i
 
-// + quantifier, matches between one and unlimited time
+// + quantifier, matches between one and unlimited time (like one word)
 // * quantifier, matches between 0 and unlimited time
+// {1,3} matches between 1 and 3
 
 console.log(regex.test(string2));
 
@@ -31,3 +32,15 @@ console.log(string2.substr(regex))
 console.log(string2.substr(regex2))
 
 // ? after a character makes it optional
+
+// \w matches 1 word
+// \n matches newline
+// \t matches tab
+
+// () matching utility
+
+let newString = "My name is Francois"
+regex = /name is ([a-zA-Z]+)/
+const match = newString.exec(regex);
+
+console.log(match);
