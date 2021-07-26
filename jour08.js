bda0e08bba1b9ec3bb212c5b3a6e4376b7d2adb5
Francois Janson <jansonfrancois@gmail.com>
2021-07-26 09:39:39 +0200

    Exercises update

--- jour08.js
+++ jour08.js
@@ -1,14 +1,60 @@
-const request = require("request");
+// 01 Countries
 
-let countriesNames = [];
+var request = require("request");
 
-request.get("https://restcountries.eu/rest/v1/all", function(err, res, body) {
-    console.log("err : " + err);
-    console.log("res: " + res);
-    // console.log("body: ");
-    // console.log(body);
+var countriesName = [];
 
-    console.log(res);
+function getCountries() {
+    request.get("https://restcountries.eu/rest/v1/all", function(err, res, body) {
+        // console.log(err);
+        // console.log(res.statusCode);
+        // console.log(body);
+        // console.log(body.length)
+
+        let countriesObject = JSON.parse(body)
+        // console.log(countriesObject["0"].name)
+
+        for (i = 0; i < countriesObject.length; i++) {
+            let index = i.toString()
+            countriesName.push(countriesObject[i].name)
+        }
+        console.log("var countriesName : ")
+        console.log(countriesName)
+    });
+}
+// getCountries()
 
 
-})
+// 02 Chuck Norris
+
+var chuckJoke = "";
+
+function getJoke() {
+    request.get("https://api.chucknorris.io/jokes/random", function(err, res, body) {
+        // console.log(body);
+
+        let chuckObject = JSON.parse(body);
+        // console.log(chuckObject);
+        chuckJoke = chuckObject.value;
+        console.log(chuckJoke);
+
+    })
+}
+getJoke()
+
+// 03 Pokemon
+
+
+function catchPokemon(nb) {
+    request.get("https://pokeapi.co/api/v2/pokemon/" + nb, function(err, res, body) {
+
+        let pokemonObject = JSON.parse(body)
+        // console.log(pokemonObject.forms[0].name);
+        let pokemonName = pokemonObject.forms[0].name;
+        console.log(`ID of the Pokemon : ${nb}`)
+        console.log(`name of the Pokemon : ${pokemonName}`)
+    })
+};
+catchPokemon(42);
+
+// Bonus BlackJack
