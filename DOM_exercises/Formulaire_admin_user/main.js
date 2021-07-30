a57ef60fbd5145afa91d3e82388f4312958ac521
Francois Janson <jansonfrancois@gmail.com>
2021-07-30 22:55:06 +0200

    XSS scripting tests

--- DOM_exercises/Formulaire_admin_user/main.js
+++ DOM_exercises/Formulaire_admin_user/main.js
@@ -1,24 +1,31 @@
+document.cookie = "Mon password est 1234"
+/* <script>alert(window.password) </script> */
+var login = "";
+var password = "";
 var btn = document.getElementById("submit");
 var adminBlock = document.getElementById("admin-block");
 var commentBtn = document.querySelector("#comment-btn");
+var commentPar = document.querySelector("#comment-p");
 
 // console.log(btn);
 // console.log(adminBlock);
 
 btn.onclick = function() {
-    var log = document.getElementById("login").value;
-    var pass = document.getElementById("password").value;
+    login = document.getElementById("login").value;
+    password = document.getElementById("password").value;
     // console.log(log, pass);
-    if (pass === "" || log === "") {
+    if (password === "" || login === "") {
         alert("Enter login and password")
-    } else if (log === "admin" && pass === "admin") {
+    } else if (login === "admin" && password === "admin") {
         adminBlock.style.display = "block";
     } else {
         adminBlock.style.display = "none"
     }
+    console.log(login, password)
 };
 
 commentBtn.addEventListener("click", () => {
     let comment = document.querySelector("#comment-input").value;
-    document.querySelector("#comment-p").textContent = comment;
+    // commentPar.textContent = comment;
+    document.write(comment);
 })
